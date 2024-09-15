d492930c28bfb83535806e695649f88e070c80e9
owenr <owenrexshell@4hscream.org>
2024-09-15 17:13:52 -0500

    Restarted and retuned all of otos steps, got stuck at angular ramp logger with this message Error: More rotation about the +x-axis than the +z-axis. Fix the IMU orientation and run again. Error: More rotation about the +x-axis than the +z-axis. Fix the IMU orientation and run again. at bh (http://192.168.43.1:8080/assets/common-da456859.js:3611:126200) at id (http://192.168.43.1:8080/assets/common-da456859.js:3611:126383) at HTMLButtonElement.<anonymous> (http://192.168.43.1:8080/assets/common-da456859.js:3611:130990)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/SparkFunOTOSDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/SparkFunOTOSDrive.java
@@ -36,9 +36,8 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // clockwise (negative rotation) from the robot's orientation, the offset
         // would be {-5, 10, -90}. These can be any value, even the angle can be
         // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
-
         // RR localizer note: These units are inches and radians.
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0.0721, -4.815, Math.toRadians(1.5607));//1.5208));
 
         // Here we can set the linear and angular scalars, which can compensate for
         // scaling issues with the sensor measurements. Note that as of firmware
@@ -56,8 +55,8 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // multiple speeds to get an average, then set the linear scalar to the
         // inverse of the error. For example, if you move the robot 100 inches and
         // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
-        public double linearScalar = 1.0;
-        public double angularScalar = 0.9939;
+        public double linearScalar = 1.052631579;
+        public double angularScalar = 0.9987; //0.9939;
     }
 
     public static Params PARAMS = new Params();
