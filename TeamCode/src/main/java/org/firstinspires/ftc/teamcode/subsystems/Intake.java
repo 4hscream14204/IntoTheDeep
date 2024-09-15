f3b7e243b39533858ea8e7f16138788421c1cd3c
PythonMaster245 <connorlithgow@4hscream.org>
2024-09-15 13:53:18 -0500

    Created an intake subsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake {
+
+    private Servo srvIntake;
+
+    public Intake (Servo conIntakeServo){
+        srvIntake = conIntakeServo;
+    }
+
+    public void intakeSpeed(double speed){
+        srvIntake.setPosition(speed);
+    }
+}
