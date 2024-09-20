8af39890ed80b6d8d27fe1438c2fd2212dd944cc
AizahR <aizrizwan16@gamil.com>
2024-09-19 20:47:06 -0500

    added class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/yoshi.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name ="yoshi")
+public class yoshi extends OpMode {
+    @Override
+     public void init(){
+
+
+    }
+    @Override
+    public void loop(){
+
+    }
+
+
+
+}
