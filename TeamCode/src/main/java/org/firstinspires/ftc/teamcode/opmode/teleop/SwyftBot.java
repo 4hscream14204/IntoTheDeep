a43b509aef8ace8e0ea99a62b9fc40c12958fd8e
micah <micahmeach@gmail.com>
2024-09-26 21:01:05 -0500

    increased slide speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/SwyftBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/SwyftBot.java
@@ -74,6 +74,10 @@ public class SwyftBot extends OpMode {
         wrist.setPosition(WRIST_FOLDED_IN);
 
  */
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addLine("Robot Ready.");
         telemetry.update();
@@ -86,14 +90,14 @@ public class SwyftBot extends OpMode {
 
     @Override
     public void loop() {
-        double y = -gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y); // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x * Math.abs(gamepad1.left_stick_x);
-        double rx = gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x);
+        double y = -gamepad1.left_stick_y * Math.abs(gamepad1.left_stick_y) * Math.abs(gamepad1.left_stick_y); // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x * Math.abs(gamepad1.left_stick_x) * Math.abs(gamepad1.left_stick_x);
+        double rx = gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x);
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 0.95);
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
@@ -103,7 +107,7 @@ public class SwyftBot extends OpMode {
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
-        telemetry.addData("Left Stick y", y);
+        telemetry.addData("Left Stick Y", y);
         telemetry.addData("Left Stick x", x);
         telemetry.addData("Right Stick x", rx);
 
