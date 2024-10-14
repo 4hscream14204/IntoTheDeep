a9bf67b2bc1bb01db0bf210c34ad394b1ab4f24c
owenr <owenrexshell@4hscream.org>
2024-10-14 10:45:17 -0500

    made lime light code opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoLimelightTest.java
@@ -1,41 +1,31 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-public class AutoLimelightTest extends LinearOpMode {
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+public class AutoLimelightTest extends OpMode {
 
     private Limelight3A limelight;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        public void init() {
+            limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
-        telemetry.setMsTransmissionInterval(11);
+            telemetry.setMsTransmissionInterval(11);
 
-        limelight.pipelineSwitch(0);
+            limelight.pipelineSwitch(0);
 
-        /*
-         * Starts polling for data.
-         */
-        limelight.start();
+            /*
+             * Starts polling for data.
+             */
+            limelight.start();
+        }
 
 
-/*
-    @Override
-    public void init() {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
-        telemetry.setMsTransmissionInterval(11);
-
-        limelight.pipelineSwitch(0);
-    }
-
-    public void start() {
-        limelight.start();
-    }
-
+@Override
     public void loop() {
             LLResult result = limelight.getLatestResult();
             if (result != null) {
@@ -44,13 +34,16 @@ public class AutoLimelightTest extends LinearOpMode {
                     telemetry.addData("tx", result.getTx());
                     telemetry.addData("ty", result.getTy());
                     telemetry.addData("Botpose", botpose.toString());
+                }
+            }
     }
 
+    @Override
     public void stop() {
 
     }
 
 
- */
-    }
+
+
 }
