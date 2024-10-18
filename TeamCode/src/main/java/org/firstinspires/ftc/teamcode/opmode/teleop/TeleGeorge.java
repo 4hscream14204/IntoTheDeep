c036dbca00de4aa524c31e7fceb1315b04b25e8e
SomeGuy568 <potatojack2777@outlook.com>
2024-10-17 20:19:27 -0500

    Better variable names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleGeorge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleGeorge.java
@@ -1,21 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.base.RobotBase;
@@ -23,21 +13,17 @@ import org.firstinspires.ftc.teamcode.commands.IntakeCommandGroup;
 import org.firstinspires.ftc.teamcode.commands.IntakeTransferCommandGroup;
 import org.firstinspires.ftc.teamcode.commands.LiftHome;
 import org.firstinspires.ftc.teamcode.commands.Liftbucketcommandgroup;
-import org.firstinspires.ftc.teamcode.subsystems.Bucket;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
-import org.firstinspires.ftc.teamcode.subsystems.Wrist;
-import org.firstinspires.ftc.teamcode.subsystems.Zlide;
 
 @TeleOp(name="George")
 public class TeleGeorge extends OpMode{
     RobotBase robotBase;
-    boolean fieldCentric  = true;
-    double denominator;
-    double frontLeftPower;
-    double backLeftPower;
-    double frontRightPower;
-    double backRightPower;
+    boolean bolFieldCentric = true;
+    double dubDenominator;
+    double dubFrontLeftPower;
+    double dubBackLeftPower;
+    double dubFrontRightPower;
+    double dubBackRightPower;
     public GamepadEx armController;
     public GamepadEx baseController;
 
@@ -59,7 +45,7 @@ public class TeleGeorge extends OpMode{
 
         baseController.getGamepadButton((GamepadKeys.Button.Y))
                 .whenPressed(()->CommandScheduler.getInstance().schedule(
-                        new InstantCommand(()->fieldCentric = !fieldCentric)
+                        new InstantCommand(()-> bolFieldCentric = !bolFieldCentric)
                 ));
 
         armController.getGamepadButton(GamepadKeys.Button.Y)
@@ -112,27 +98,27 @@ public class TeleGeorge extends OpMode{
         double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
         double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-        if (fieldCentric) {
+        if (bolFieldCentric) {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            frontLeftPower = (rotY + rotX + rx) / denominator;
-            backLeftPower = (rotY - rotX + rx) / denominator;
-            frontRightPower = (rotY - rotX - rx) / denominator;
-            backRightPower = (rotY + rotX - rx) / denominator;
+            dubDenominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            dubFrontLeftPower = (rotY + rotX + rx) / dubDenominator;
+            dubBackLeftPower = (rotY - rotX + rx) / dubDenominator;
+            dubFrontRightPower = (rotY - rotX - rx) / dubDenominator;
+            dubBackRightPower = (rotY + rotX - rx) / dubDenominator;
         } else {
-            denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            frontLeftPower = (y + x + rx) / denominator;
-            backLeftPower = (y - x + rx) / denominator;
-            frontRightPower = (y - x - rx) / denominator;
-            backRightPower = (y + x - rx) / denominator;
+            dubDenominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            dubFrontLeftPower = (y + x + rx) / dubDenominator;
+            dubBackLeftPower = (y - x + rx) / dubDenominator;
+            dubFrontRightPower = (y - x - rx) / dubDenominator;
+            dubBackRightPower = (y + x - rx) / dubDenominator;
         }
 
-        robotBase.frontLeftMotor.setPower(frontLeftPower);
-        robotBase.backLeftMotor.setPower(backLeftPower);
-        robotBase.frontRightMotor.setPower(frontRightPower);
-        robotBase.backRightMotor.setPower(backRightPower);
+        robotBase.frontLeftMotor.setPower(dubFrontLeftPower);
+        robotBase.backLeftMotor.setPower(dubBackLeftPower);
+        robotBase.frontRightMotor.setPower(dubFrontRightPower);
+        robotBase.backRightMotor.setPower(dubBackRightPower);
         telemetry.addData("Left Stick y", y);
         telemetry.addData("Left Stick x", x);
         telemetry.addData("Right Stick rx", rx);
@@ -147,7 +133,7 @@ public class TeleGeorge extends OpMode{
 
         telemetry.addData("Lift motor power", robotBase.liftSubsystem.getPower());
 
-        telemetry.addData("feild centric", fieldCentric);
+        telemetry.addData("field centric", bolFieldCentric);
 
         telemetry.addData("Gyro", robotBase.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
 
