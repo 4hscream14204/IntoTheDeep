dd24355727c5c750502ab828e2f2724031fecb59
SomeGuy568 <potatojack2777@outlook.com>
2024-10-17 20:46:35 -0500

    changed a bucket value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Bucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Bucket.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Bucket extends SubsystemBase {
 
-    double bucketUpPosition = 0;
+    double bucketUpPosition = 0.407222;
     double bucketDownPosition = 0.79333;
 
      Servo srvBucket;
