5352b2a4680f1c7932b44ff20c8070b16622ec1d
PythonMaster245 <connorlithgow@4hscream.org>
2024-10-26 17:06:51 -0500

    Started working on auto route.

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepConnor.java
@@ -0,0 +1,36 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepConnor {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-25, 61, Math.toRadians(-90)))
+                .splineToConstantHeading(new Vector2d(-12, 41), Math.toRadians(-90.00))
+                .splineToConstantHeading(new Vector2d(-34, 34), Math.toRadians(180.00))
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(-35, 10), Math.toRadians(-90.00))
+                .splineToConstantHeading(new Vector2d(-44, 0), Math.toRadians(-90.00))
+                .splineToConstantHeading(new Vector2d(-48, 58), Math.toRadians(90.00))
+                .splineToConstantHeading(new Vector2d(-44, 6), Math.toRadians(-90.00))
+                .splineToConstantHeading(new Vector2d(-56, 7), Math.toRadians(180.00))
+                .splineToConstantHeading(new Vector2d(-30, 60), Math.toRadians(90.00))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
