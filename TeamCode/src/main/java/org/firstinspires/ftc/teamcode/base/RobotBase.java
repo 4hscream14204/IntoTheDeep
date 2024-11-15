e8fe24dce3c13624c9a65c773b31327195e717c0
SomeGuy568 <potatojack2777@outlook.com>
2024-11-14 19:33:58 -0600

    added wheels to robot base, but more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
@@ -9,5 +9,8 @@ public class RobotBase {
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
 
-
+    frontLeftMotor = hwMap.dcMotor.get("left_front");
+    backLeftMotor = hwMap.dcMotor.get("left_back");
+    frontRightMotor = hwMap.dcMotor.get("right_front");
+    backRightMotor = hwMap.dcMotor.get("right_back");
 }
