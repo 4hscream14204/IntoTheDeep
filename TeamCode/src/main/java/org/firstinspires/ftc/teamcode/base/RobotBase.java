9f56270ecdf1a40d11ee09d84544eb332c1f3360
SomeGuy568 <potatojack2777@outlook.com>
2024-11-14 19:32:43 -0600

    added wheels to robot base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.base;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class RobotBase {
+
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor backRightMotor;
+
+
 }
