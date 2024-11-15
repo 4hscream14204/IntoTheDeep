eb2132e66530c6ce19f2aa8e2419a5cf9a41083f
SomeGuy568 <potatojack2777@outlook.com>
2024-11-14 19:52:50 -0600

    shoulder and extension added to base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.base;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotBase {
 
@@ -8,9 +10,16 @@ public class RobotBase {
     public DcMotor frontRightMotor;
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
+    public DcMotor shoulder;
+    public DcMotor extension;
 
-    frontLeftMotor = hwMap.dcMotor.get("left_front");
-    backLeftMotor = hwMap.dcMotor.get("left_back");
-    frontRightMotor = hwMap.dcMotor.get("right_front");
-    backRightMotor = hwMap.dcMotor.get("right_back");
+    public RobotBase(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.dcMotor.get("left_front");
+        backLeftMotor = hwMap.dcMotor.get("left_back");
+        frontRightMotor = hwMap.dcMotor.get("right_front");
+        backRightMotor = hwMap.dcMotor.get("right_back");
+        extension = hwMap.dcMotor.get("extension");
+        shoulder = hwMap.dcMotor.get("shoulder");
+
+    }
 }
