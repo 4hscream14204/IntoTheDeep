5a5576e9441dc7ccde1afac3bb35a8640757ce33
PythonMaster245 <connorlithgow@4hscream.org>
2024-11-14 21:06:06 -0600

    Working on issue with Lift, not fixed yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftHome.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftHome.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
@@ -12,13 +13,19 @@ public class LiftHome extends SequentialCommandGroup {
     public LiftHome(Lift lift, Wrist wrist) {
         if(wrist.getPosition() == wrist.wristTransfer){
             addCommands(
-                    new InstantCommand(wrist::wristPickupPos)
+                    new InstantCommand(wrist::wristPickupPos),
+                    new WaitCommand(500),
+                    new InstantCommand(() -> lift.goDown(lift.downPower)),
+                    new WaitUntilCommand(lift::getSwitchState),
+                    new InstantCommand(lift::reset)
             );
         }
-        addCommands(
-                new InstantCommand(()->lift.goDown(lift.downPower)),
-                new WaitUntilCommand(lift::getSwitchState),
-                new InstantCommand(lift::reset)
-        );
+        else {
+            addCommands(
+                    new InstantCommand(() -> lift.goDown(lift.downPower)),
+                    new WaitUntilCommand(lift::getSwitchState),
+                    new InstantCommand(lift::reset)
+            );
+        }
+        }
     }
-}
