9ada4ace0f6e9eac71039b3ecec0e39735ea12aa
SomeGuy568 <potatojack2777@outlook.com>
2024-11-16 15:37:19 -0600

    this is a commit message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
@@ -5,6 +5,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Wrist extends SubsystemBase {
 
+    public enum WristPosition{
+        HOME (0);
+        public final double position;
+        WristPosition(double m_position) {
+            this.position = m_position;
+        }
+    }
+
+    Servo srvWrist;
+
     public Wrist(Servo wristServo) {
+    srvWrist = wristServo;
+    srvWrist.setPosition(WristPosition.HOME.position);
+    }
+
+    public void goToPosition() {
+srvWrist.setPosition()
+
     }
 }
