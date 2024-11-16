396515df65bdf5aa5c1116b5f7e92f5b6f07a094
PythonMaster245 <connorlithgow@4hscream.org>
2024-11-16 14:26:25 -0600

    Finished basic shoulder functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shoulder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shoulder.java
@@ -11,6 +11,7 @@ public class Shoulder extends SubsystemBase {
     public DigitalChannel tsShoulderLimitSwitch;
     public double upPower = -0.3;
     public double downPower = 0.3;
+    public boolean stoppedInPlace = true;
 
     public Shoulder(DcMotor conShoulderMotor, DigitalChannel conShoulderLimitSwitch) {
         shoulderMotor = conShoulderMotor;
@@ -33,7 +34,26 @@ public class Shoulder extends SubsystemBase {
             shoulderMotor.setPower(upPower);
         }
         else{
+            stopInPlace();
             return;
+        }
+        shoulderMotor.setTargetPosition(enmTargetPosition.height);
+        shoulderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        stoppedInPlace = false;
+    }
+
+    public void stopInPlace(){
+        if(stoppedInPlace){
+            return;
+        }
+        if(isShoulderDown()){
+            reset();
+        }
+        else{
+            shoulderMotor.setPower(upPower);
+            shoulderMotor.setTargetPosition(shoulderMotor.getCurrentPosition());
+            shoulderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
     }
 
@@ -43,5 +63,13 @@ public class Shoulder extends SubsystemBase {
 
     public boolean isShoulderDown(){
        return tsShoulderLimitSwitch.getState();
+    }
+
+    public void reset(){
+        shoulderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shoulderMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shoulderMotor.setTargetPosition(0);
+        shoulderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        shoulderMotor.setPower(0);
     }
 }
