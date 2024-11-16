0275baefe0a03c39f6c7a4bec77f3bbdc6db89f2
PythonMaster245 <connorlithgow@4hscream.org>
2024-11-16 16:03:16 -0600

    Added key methods to elbow and shoulder.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
@@ -4,20 +4,38 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Elbow extends SubsystemBase {
+
+    public enum ElbowPosition {
+        HOME (0);
+        public final int value;
+        ElbowPosition(int m_pos){
+            this.value = m_pos;
+        }
+    }
+
     public Servo elbowServo;
-    public double homeElbowServoPosition = 0;
-    public double pickupElbowServoPosition = 0;
+
+    public ElbowPosition enmElbowPosition;
 
     public Elbow(Servo conElbowServo) {
         elbowServo = conElbowServo;
-        elbowServo.setPosition(homeElbowServoPosition);
+        goToPosition(ElbowPosition.HOME);
+        enmElbowPosition = ElbowPosition.HOME;
     }
 
-    public void elbowHomePosition(){
-        elbowServo.setPosition(homeElbowServoPosition);
+    public boolean isAtPosition(ElbowPosition elbowTarget){
+        if(Math.abs(elbowServo.getPosition() - elbowTarget.value) <= 0.05){
+            return true;
+        }
+        return false;
     }
 
-    public void elbowPickupPosition(){
-        elbowServo.setPosition(pickupElbowServoPosition);
+    public void goToPosition(ElbowPosition m_position){
+        enmElbowPosition = ElbowPosition.HOME;
+        elbowServo.setPosition(m_position.value);
+    }
+
+    public double getPosition(){
+        return elbowServo.getPosition();
     }
 }
