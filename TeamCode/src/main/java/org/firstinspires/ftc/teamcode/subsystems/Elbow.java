29b826925dedbea55aaf5b9c2c8a53340f04be24
PythonMaster245 <connorlithgow@4hscream.org>
2024-11-16 15:29:14 -0600

    Added basic elbow functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
@@ -4,8 +4,20 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Elbow extends SubsystemBase {
+    public Servo elbowServo;
+    public double homeElbowServoPosition = 0;
+    public double pickupElbowServoPosition = 0;
 
-    public Elbow(Servo elbowServo) {
+    public Elbow(Servo conElbowServo) {
+        elbowServo = conElbowServo;
+        elbowServo.setPosition(homeElbowServoPosition);
+    }
 
+    public void elbowHomePosition(){
+        elbowServo.setPosition(homeElbowServoPosition);
+    }
+
+    public void elbowPickupPosition(){
+        elbowServo.setPosition(pickupElbowServoPosition);
     }
 }
