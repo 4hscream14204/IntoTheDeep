96a9347e7e3eff25c4120521da12389a86ebee33
owenr <owenrexshell@4hscream.org>
2024-12-03 20:08:17 -0600

    tested limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoLimelightTest.java
@@ -1,49 +1,64 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.List;
 
 public class AutoLimelightTest extends OpMode {
 
     private Limelight3A limelight;
 
     @Override
-        public void init() {
-            limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
-            telemetry.setMsTransmissionInterval(11);
+        telemetry.setMsTransmissionInterval(11);
 
-            limelight.pipelineSwitch(0);
+        limelight.pipelineSwitch(0);
 
-            /*
-             * Starts polling for data.
-             */
-            limelight.start();
-        }
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
 
-
-@Override
-    public void loop() {
-            LLResult result = limelight.getLatestResult();
-            if (result != null) {
-                if (result.isValid()) {
-                    Pose3D botpose = result.getBotpose();
-                    telemetry.addData("tx", result.getTx());
-                    telemetry.addData("ty", result.getTy());
-                    telemetry.addData("Botpose", botpose.toString());
-                }
-            }
     }
+
 
     @Override
-    public void stop() {
+    public void loop() {
+        LLResult result = limelight.getLatestResult();
+        if (result != null) {
+            if (result.isValid()) {
+                Pose3D botpose = result.getBotpose();
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("Botpose", botpose.toString());
+            }
+        }
+        if (result.isValid()) {
+            // Access fiducial results
+            List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+            }
+
+
+        }
+/*
+        @Override
+        public void stop () {
+
+        }
+
+ */
+
 
     }
-
-
-
-
 }
