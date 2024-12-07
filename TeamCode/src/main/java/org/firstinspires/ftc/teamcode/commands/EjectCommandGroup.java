1c811eeb7b42c7fda7eea36b693cebeb720f5614
SomeGuy568 <potatojack2777@outlook.com>
2024-12-07 16:07:58 -0600

    Eject command group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
@@ -1,10 +1,18 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.base.RobotBase;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Wrist;
 
 public class EjectCommandGroup extends SequentialCommandGroup {
-
+    public EjectCommandGroup(Intake intake){
+        addCommands(
+                new InstantCommand(()-> intake.gateGoToPosition(Intake.GatePosition.OPEN)),
+                new InstantCommand(()-> intake.intakeOuttake()),
+                new InstantCommand(()-> intake.gateGoToPosition(Intake.GatePosition.ClOSED))
+        );
+    }
 }
