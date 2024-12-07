1b63ab1f32fc18eab6bb43262e58af1686e41570
SomeGuy568 <potatojack2777@outlook.com>
2024-12-07 15:52:41 -0600

    switching to george

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.base.RobotBase;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+public class EjectCommandGroup extends SequentialCommandGroup {
+
+}
