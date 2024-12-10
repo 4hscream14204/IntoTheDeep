2630a60345762b5258b0b906f798289f799d91ba
SomeGuy568 <potatojack2777@outlook.com>
2024-12-09 21:41:06 -0600

    uncommited change?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/CrabTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/CrabTeleOp.java
@@ -215,7 +215,7 @@ public class CrabTeleOp extends OpMode {
             robotBase.extensionSubsystem.extendBack(gamepad1.right_trigger);
         }
 
-        if(gamepad1.right_trigger <= 0.1 && gamepad1.left_trigger <= 0.1){
+        if(!robotBase.extensionSubsystem.bolHasStopRan){
             robotBase.extensionSubsystem.stopInPlace();
         }
         //Connor: I don't think we need these but I commented them just in case.
