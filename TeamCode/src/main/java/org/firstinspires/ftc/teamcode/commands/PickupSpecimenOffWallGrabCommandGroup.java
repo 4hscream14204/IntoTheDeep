91ea733e1800cb9c0222839f1875bb245d1815f7
PythonMaster245 <connorlithgow@4hscream.org>
2024-12-27 16:21:22 -0600

    Created specimen pickup command groups that are toggleable. Not added to main opmode yet.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/PickupSpecimenOffWallGrabCommandGroup.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Elbow;
+import org.firstinspires.ftc.teamcode.subsystems.Extension;
+import org.firstinspires.ftc.teamcode.subsystems.Shoulder;
+import org.firstinspires.ftc.teamcode.subsystems.Wrist;
+
+public class PickupSpecimenOffWallGrabCommandGroup extends SequentialCommandGroup {
+    public PickupSpecimenOffWallGrabCommandGroup(Extension extension, Shoulder shoulder, Claw claw, Elbow elbow, Wrist wrist){
+        addCommands(
+                new InstantCommand(claw::closeClaw),
+                new WaitCommand(250),
+                new ShoulderHomeCommandGroup(shoulder)
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/PickupSpecimenOffWallLineUpCommandGroup.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Elbow;
+import org.firstinspires.ftc.teamcode.subsystems.Extension;
+import org.firstinspires.ftc.teamcode.subsystems.Shoulder;
+import org.firstinspires.ftc.teamcode.subsystems.Wrist;
+
+public class PickupSpecimenOffWallLineUpCommandGroup extends SequentialCommandGroup {
+    public PickupSpecimenOffWallLineUpCommandGroup(Extension extension, Shoulder shoulder, Elbow elbow, Wrist wrist, Claw claw){
+        addCommands(
+                new ExtensionHomeCommandGroup(extension, elbow),
+                new InstantCommand(()->wrist.goToPosition(Wrist.WristPosition.PRESUBPICKUP)),
+                new InstantCommand(()->elbow.goToPosition(Elbow.ElbowPosition.PRESUBPICKUP)),
+                new InstantCommand(claw::openClaw),
+                new InstantCommand(()->shoulder.goToPosition(Shoulder.ShoulderPosition.TOGGLE))
+        );
+    }
+}
