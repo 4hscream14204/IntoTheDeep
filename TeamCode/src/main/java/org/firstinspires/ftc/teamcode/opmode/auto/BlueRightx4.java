17c445ce435016d262b8176affa97cbd657c0062
owenr <owenrexshell@4hscream.org>
2025-01-12 15:27:12 -0600

    finished tuning movements for blue right moving onto arm movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRightx4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueRightx4.java
@@ -37,7 +37,7 @@ public class BlueRightx4 extends OpMode {
 
     @Override
     public void init() {
-        startPose = new Pose2d(-14, 61, Math.toRadians(270));
+        startPose = new Pose2d(-14, 61, Math.toRadians(0));
         robotBase =new RobotBase(hardwareMap);
         armController = new GamepadEx(gamepad2);
         baseController = new GamepadEx(gamepad1);
@@ -58,50 +58,57 @@ public class BlueRightx4 extends OpMode {
 
         blueRightx4Action = robotBase.drive.actionBuilder(startPose)
                 .setTangent(Math.toRadians(270))
-                .splineToConstantHeading(new Vector2d(-6.0, 26.00), Math.toRadians(270.00), new TranslationalVelConstraint(20))
-                //    .setTangent(90)
+                .splineToConstantHeading(new Vector2d(-6.0, 30.00), Math.toRadians(270.00), new TranslationalVelConstraint(40))
+
                 //line up for first specimen
                 .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-6.0, 30), Math.toRadians(90), new TranslationalVelConstraint(20))
+                .splineToConstantHeading(new Vector2d(-6.0, 35), Math.toRadians(90), new TranslationalVelConstraint(40))
+                //30
                 // .setTangent(90)
-                .splineToLinearHeading(new Pose2d(-34.00, 35.00, Math.toRadians(130.00)), Math.toRadians(180.00), new TranslationalVelConstraint(20))
+                .splineToLinearHeading(new Pose2d(-34.00, 36.00, Math.toRadians(130.00)), Math.toRadians(180.00), new TranslationalVelConstraint(30))
                 .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(new Pose2d(-35.00, 40, Math.toRadians(50)), Math.toRadians(90), new TranslationalVelConstraint(20))
+                .splineToLinearHeading(new Pose2d(-38.00, 40, Math.toRadians(50)), Math.toRadians(90), new TranslationalVelConstraint(30))
                 //  .splineToLinearHeading(new Pose2d(-35.96, 40.00, Math.toRadians(50.00)), Math.toRadians(120.00), new TranslationalVelConstraint(20))
                 .setTangent(Math.toRadians(200))
-                .splineToLinearHeading(new Pose2d(-44.00, 34.00, Math.toRadians(120.00)), Math.toRadians(200.00), new TranslationalVelConstraint(20))
+                .splineToLinearHeading(new Pose2d(-42.00, 34.00, Math.toRadians(120.00)), Math.toRadians(200.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
 
-                .splineToLinearHeading(new Pose2d(-45.90, 41.00, Math.toRadians(50.00)), Math.toRadians(120.00), new TranslationalVelConstraint(20))
+                .splineToLinearHeading(new Pose2d(-47.00, 41.00, Math.toRadians(50.00)), Math.toRadians(120.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
-                .setTangent(Math.toRadians(120))
-                .splineToLinearHeading(new Pose2d(-50.00, 34.00, Math.toRadians(120.00)), Math.toRadians(200.00), new TranslationalVelConstraint(20))
+                /*
+                .setTangent(Math.toRadians(200))
+                .splineToLinearHeading(new Pose2d(-48.00, 34.00, Math.toRadians(120.00)), Math.toRadians(200.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
-                .splineToLinearHeading(new Pose2d(-58.00, 39.52, Math.toRadians(20.00)), Math.toRadians(120.00), new TranslationalVelConstraint(20))
+                .splineToLinearHeading(new Pose2d(-52.00, 39.00, Math.toRadians(20.00)), Math.toRadians(120.00), new TranslationalVelConstraint(30))
+
+                 */
                 .waitSeconds(0.1)
-                .splineToSplineHeading(new Pose2d(-49.00, 55.84, Math.toRadians(180.00)), Math.toRadians(87.06), new TranslationalVelConstraint(20))
-                .splineToConstantHeading(new Vector2d(-49.00, 60.00), Math.toRadians(90.00), new TranslationalVelConstraint(20))
+                .setTangent(45)
+                .splineToLinearHeading(new Pose2d(-45.00, 50.00, Math.toRadians(180.00)), Math.toRadians(87.06), new TranslationalVelConstraint(30))
+                .splineToConstantHeading(new Vector2d(-49, 60.00), Math.toRadians(90.00), new TranslationalVelConstraint(20))
                 .waitSeconds(0.1)
                 .setTangent(Math.toRadians(270))
-                .splineToLinearHeading(new Pose2d(-6.00, 29.00, Math.toRadians(180.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
+                .splineToLinearHeading(new Pose2d(-5.00, 29.00, Math.toRadians(0.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
                 .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-49.00, 56.00), Math.toRadians(90.00), new TranslationalVelConstraint(30))
+                .splineToLinearHeading(new Pose2d(-49.00, 48.00, Math.toRadians(180.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
                 .splineToConstantHeading(new Vector2d(-49, 60.00), Math.toRadians(90.00), new TranslationalVelConstraint(20))
                 .waitSeconds(0.1)
                 .setTangent(Math.toRadians(320))
-                .splineToConstantHeading(new Vector2d(-5.60, 28.70), Math.toRadians(270.00), new TranslationalVelConstraint(30))
+                .splineToLinearHeading(new Pose2d(-4.0, 29.00, Math.toRadians(0.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
-                .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-49.31, 55.98), Math.toRadians(90.00), new TranslationalVelConstraint(30))
+               /* .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(-49.00, 52.00, Math.toRadians(180.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
                 .splineToConstantHeading(new Vector2d(-49, 60.00), Math.toRadians(90.00), new TranslationalVelConstraint(20))
                 .waitSeconds(0.1)
                 .setTangent(Math.toRadians(320))
-                .splineToConstantHeading(new Vector2d(-2.00, 28.99), Math.toRadians(270.00), new TranslationalVelConstraint(30))
+                .splineToLinearHeading(new Pose2d(-2.60, 29.00, Math.toRadians(0.00)) , Math.toRadians(270.00), new TranslationalVelConstraint(30))
                 .waitSeconds(0.1)
+
+                */
                 .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-61.81, 62.66), Math.toRadians(90.00), new TranslationalVelConstraint(30))
-                .build();
+                .splineToConstantHeading(new Vector2d(-58.00, 60.66), Math.toRadians(120.00), new TranslationalVelConstraint(40))
+                                .build();
 
         //robotBase.alliance = ITDCrabEnums.EnmAlliance.BLUE;
 
