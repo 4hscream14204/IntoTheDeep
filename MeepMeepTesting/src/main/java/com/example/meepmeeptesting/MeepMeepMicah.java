5f5186f77efbac0a01cadaa070b28e005c8a9361
micah <micahmeach@gmail.com>
2025-01-14 20:09:09 -0600

    test

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMicah.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMicah.java
@@ -3,6 +3,8 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -16,22 +18,23 @@ public class MeepMeepMicah {
 
                 .build();
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(14, 61, Math.toRadians(180)))
-                        .waitSeconds(2)
-                        .setTangent(Math.toRadians(270))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(270))
                 .splineToConstantHeading(new Vector2d(8, 34), Math.toRadians(270), new TranslationalVelConstraint(20))
-                        .setTangent(Math.toRadians(90))
+                  .afterTime(2, (()-> CommandScheduler.getInstance().schedule(new InstantCommand(()->robotBase.)
+                .setTangent(Math.toRadians(90))
                 .splineToLinearHeading(new Pose2d(40, 33, Math.toRadians(230)), Math.toRadians(310))
-                                .setTangent(Math.toRadians(55))
-            //    .splineToConstantHeading(new Vector2d(40.12, 33.44), Math.toRadians(-12.77))
+                .setTangent(Math.toRadians(55))
+            //  .splineToConstantHeading(new Vector2d(40.12, 33.44), Math.toRadians(-12.77))
                 .splineToLinearHeading(new Pose2d(50, 56, Math.toRadians(-42)), Math.toRadians(59))
-                        .setTangent(Math.toRadians(250))
+                .setTangent(Math.toRadians(250))
                 .splineToLinearHeading(new Pose2d(52, 29, Math.toRadians(180)), Math.toRadians(-87), new TranslationalVelConstraint(20))
-                        .setTangent(Math.toRadians(45))
+                .setTangent(Math.toRadians(45))
                 .splineToLinearHeading(new Pose2d(50, 56, Math.toRadians(317)), Math.toRadians(55), new TranslationalVelConstraint(20))
-                                .setTangent(Math.toRadians(270))
+                .setTangent(Math.toRadians(270))
                // .splineTo(new Vector2d(67.55, 56.43), Math.toRadians(254.77))
                 .splineToLinearHeading(new Pose2d(54, 31, Math.toRadians(180)), Math.toRadians(45), new TranslationalVelConstraint(20))
-                        .setTangent(Math.toRadians(45))
+                .setTangent(Math.toRadians(45))
                 .splineToLinearHeading(new Pose2d(57, 50, Math.toRadians(320)), Math.toRadians(45), new TranslationalVelConstraint(20))
                 .setTangent(Math.toRadians(270))
                 .splineTo(new Vector2d(50, 19), Math.toRadians(220), new TranslationalVelConstraint(20))
