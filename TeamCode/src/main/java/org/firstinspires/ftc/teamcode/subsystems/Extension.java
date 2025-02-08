04aa937adade02d0720d31679a990191e2810116
micah <micahmeach@gmail.com>
2025-02-08 14:47:38 -0600

    value change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Extension.java
@@ -116,7 +116,7 @@ public class Extension extends SubsystemBase {
     }
 
     public boolean isAtPosition(ExtensionPosition targetPosition){
-        if(Math.abs(extendMotor.getCurrentPosition() - targetPosition.height) <= 40){
+        if(Math.abs(extendMotor.getCurrentPosition() - targetPosition.height) <= 50){
             return true;
         }
         return false;
