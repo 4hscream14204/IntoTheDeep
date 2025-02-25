73818b906831cc39d6675945ab06d390ca9c97a5
micah <micahmeach@gmail.com>
2025-02-24 20:43:50 -0600

    5 specimen auto both

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elbow.java
@@ -11,6 +11,7 @@ public class Elbow extends SubsystemBase {
         PICKUP (0.9666666666666667),
         MAX (1),
         MIN (0),
+        AUTO (0.1888),
         PRESUBPICKUP (0.6777),
         DROPOFF (0.777),
         GIVETOHUMAN (0.6777);
