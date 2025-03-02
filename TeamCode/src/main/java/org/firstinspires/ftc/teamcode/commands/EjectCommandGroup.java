48ef1ba5bedfd6238e28529cdfee8bea999d0cd3
PythonMaster245 <connorlithgow@4hscream.org>
2025-03-01 23:19:25 -0600

    Made changes at qualifier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
@@ -39,12 +39,12 @@ public class EjectCommandGroup extends SequentialCommandGroup {
                     new InstantCommand(() -> robotBase.intakeSubsystem.intakeSpeed(1)),
                     new WaitCommand(200),
                     new InstantCommand(() -> robotBase.intakeSubsystem.intakeStop()),
-                    new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.ClOSED)),
                     new InstantCommand(()-> robotBase.elbowSubsystem.goToPosition(Elbow.ElbowPosition.PICKUP)),
                     new InstantCommand(()->robotBase.extensionSubsystem.goToPosition(Extension.ExtensionPosition.HOME)),
                     new WaitUntilCommand(()->robotBase.extensionSubsystem.extensionGetPosition() > (Extension.ExtensionPosition.SECONDLEVELASCENTPULL.height)),
                     new ParallelCommandGroup(new ShoulderHomeCommandGroup(robotBase.shoulderSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem),
-                            new ExtensionHomeCommandGroup(robotBase.extensionSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem))
+                            new ExtensionHomeCommandGroup(robotBase.extensionSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem)),
+                    new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.ClOSED))
             );
         }
         else if(DataStorage.strategy == ITDCrabEnums.Strategy.SPECIMENBASICCYCLE){
@@ -61,13 +61,13 @@ public class EjectCommandGroup extends SequentialCommandGroup {
             addCommands(
                     new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.OPEN)),
                     new InstantCommand(() -> robotBase.intakeSubsystem.intakeOuttake()),
-                    new WaitCommand(200),
+                    new WaitCommand(500),
                     new InstantCommand(() -> robotBase.intakeSubsystem.intakeStop()),
                     new WaitCommand(500),
                     new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.ClOSED)),
                     new InstantCommand(()-> robotBase.elbowSubsystem.goToPosition(Elbow.ElbowPosition.PICKUP)),
                     new InstantCommand(()->robotBase.extensionSubsystem.goToPosition(Extension.ExtensionPosition.HOME)),
-                    new WaitUntilCommand(()->robotBase.extensionSubsystem.extensionGetPosition() > (Extension.ExtensionPosition.SECONDLEVELASCENTPULL.height)),
+                    new WaitUntilCommand(()->robotBase.extensionSubsystem.extensionGetPosition() > (Extension.ExtensionPosition.HIGHCHAMBERCLAMP.height)),
                     new ParallelCommandGroup(new ShoulderHomeCommandGroup(robotBase.shoulderSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem),
                             new ExtensionHomeCommandGroup(robotBase.extensionSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem))
             );
