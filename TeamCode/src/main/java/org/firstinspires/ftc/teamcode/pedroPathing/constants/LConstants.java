9770c37f3b16176fe994c9f6ff307a3bf7494d8f
PythonMaster245 <connorlithgow@4hscream.org>
2025-03-06 21:03:41 -0600

    Started to work on implementing Pedro Pathing. Got errors trying to upload.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -2,9 +2,20 @@ package org.firstinspires.ftc.teamcode.pedroPathing.constants;
 
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
+        OTOSConstants.useCorrectedOTOSClass = true;
+        OTOSConstants.hardwareMapName = "sensor_otos";
+        OTOSConstants.linearUnit = DistanceUnit.INCH;
+        OTOSConstants.angleUnit = AngleUnit.RADIANS;
+        OTOSConstants.offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0.0134));
+        OTOSConstants.linearScalar = 1 /*0.9617604062*/;
+        OTOSConstants.angularScalar = 1 /*1.001*/;
         ThreeWheelConstants.forwardTicksToInches = .001989436789;
         ThreeWheelConstants.strafeTicksToInches = .001989436789;
         ThreeWheelConstants.turnTicksToInches = .001989436789;
