cbd573903d7f4b3766f9ecb55f1e9c56b1326c91
PythonMaster245 <connorlithgow@4hscream.org>
2025-06-12 09:03:13 -0500

    Added if statement to eject command group to work with new stockpile plan.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/EjectCommandGroup.java
@@ -43,6 +43,21 @@ public class EjectCommandGroup extends SequentialCommandGroup {
                     new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.CLOSED))
             );
         }
+        else if(DataStorage.strategy == ITDCrabEnums.Strategy.SPECIMENSTOCKPILE && robotBase.intakeSubsystem.isRightColor() && robotBase.shoulderSubsystem.isShoulderHome()){
+            addCommands(
+                    new InstantCommand(()->robotBase.ledSubsystem.intSpecimensToDeliver ++),
+                    //new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.OPEN)),
+                    new InstantCommand(() -> robotBase.intakeSubsystem.intakeSpeed(0)),
+                    new WaitCommand(200),
+                    new InstantCommand(() -> robotBase.intakeSubsystem.intakeStop())
+                    /*new InstantCommand(()-> robotBase.elbowSubsystem.goToPosition(Elbow.ElbowPosition.PICKUP)),
+                    new InstantCommand(()->robotBase.extensionSubsystem.goToPosition(Extension.ExtensionPosition.HOME)),
+                    new WaitUntilCommand(()->robotBase.extensionSubsystem.extensionGetPosition() > (Extension.ExtensionPosition.SECONDLEVELASCENTPULL.height)),
+                    new ParallelCommandGroup(new ShoulderHomeCommandGroup(robotBase.shoulderSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem),
+                            new ExtensionHomeCommandGroup(robotBase.extensionSubsystem, robotBase.elbowSubsystem, robotBase.wristSubsystem)),
+                    new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.CLOSED))*/
+            );
+        }
         else if(DataStorage.strategy == ITDCrabEnums.Strategy.SPECIMENBASICCYCLE){
             addCommands(
                     new InstantCommand(() -> robotBase.intakeSubsystem.gateGoToPosition(Intake.GatePosition.OPEN)),
