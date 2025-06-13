927dead4fb4683d92c1f4b81a73edf76ca89f87c
PythonMaster245 <connorlithgow@4hscream.org>
2025-06-13 09:02:02 -0500

    Made it so gamepad2 rumbles when endgame starts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/CrabTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/CrabTeleOp.java
@@ -283,7 +283,7 @@ public class CrabTeleOp extends OpMode {
                 .whenActive(()->CommandScheduler.getInstance().schedule(new InstantCommand(()->gamepad1.rumble(30, 30, 500))));
 
         new Trigger(()->robotBase.ledSubsystem.endgameRumble(dblCurrentTime))
-                .whenActive(()->CommandScheduler.getInstance().schedule(new InstantCommand(()->gamepad1.rumble(1000)), new InstantCommand(()-> gamepad1.rumble(1000)), new InstantCommand(()->gamepad1.runLedEffect(endgameEffect)), new InstantCommand(()->gamepad1.runLedEffect(endgameEffect))));
+                .whenActive(()->CommandScheduler.getInstance().schedule(new InstantCommand(()->gamepad1.rumble(1000)), new InstantCommand(()-> gamepad2.rumble(1000)), new InstantCommand(()->gamepad1.runLedEffect(endgameEffect)), new InstantCommand(()->gamepad1.runLedEffect(endgameEffect))));
 
        /* new Trigger(()->robotBase.intakeSubsystem.isBlueSample() && DataStorage.alliance == ITDCrabEnums.EnmAlliance.RED)
                 .whenActive(
